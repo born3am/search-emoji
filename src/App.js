b4ddcd785ecc822d16ed7f28351cf8408082c3d6
born3am <psmaranhao@gmail.com>
2021-11-03 11:21:47 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import EmojiContainer from './components/EmojiContainer';
 import Header from './components/Header';
 import data from "./json/emoji.json"
 
 function App() {
 
-  const [emojisList, setEmojisList]= useState(data)
+  const [emojisList, setEmojisList] = useState(data)
 
 
   const searchEmojis = (e) => {
@@ -14,15 +14,22 @@ function App() {
 
     setEmojisList(searchedEmojis)
 
-  } 
+  }
 
   return (
     <div className="App">
-    <Header />
-    <div style={{textAlign: "center"}} >
-      <input type="text" name="search" onChange={searchEmojis} />
-    </div>
-    <EmojiContainer emojisList={emojisList} />
+      <Header />
+      <div style={{ textAlign: "center" }} >
+        <input style={{
+          display: "inline-block",
+          width: "500px",
+          borderRadius: "10px",
+          fontSize: "18px",
+          padding: "10px",
+          outline:"none"
+        }} type="text" name="search" onChange={searchEmojis} />
+      </div>
+      <EmojiContainer emojisList={emojisList} />
 
 
     </div>
--- src/components/EmojiItem.js
+++ src/components/EmojiItem.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import ReactClipboard from "react-clipboard-simple"
 
 export default function EmojiItem(props) {
     return (
-        <div>
-            <h1>{props.symbol}</h1>
-        </div>
+        <ReactClipboard keyword= {props.symbol} >
+            <div style={{ width: "200px", height: "200px", display: "grid", placeItems: "center", boxShadow: "2px 5px 5px gray", borderRadius: "20px", border: "1px solid gray", margin: "10px", fontSize: "25px" }} >
+                <h1>{props.symbol}</h1>
+            </div>
+        </ReactClipboard>
     )
 }
