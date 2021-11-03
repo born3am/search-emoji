83ba36326ea940c188ae1085104933f5a3de3ba1
born3am <psmaranhao@gmail.com>
2021-11-03 09:31:48 +0100

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Header() {
     return (
-        <div>
+        <div style={{textAlign:"center"}} >
             <h1>React Search Emoji <span>&#x1F984;</span></h1>
             <p>Simple tool for search emoji with React</p>
         
