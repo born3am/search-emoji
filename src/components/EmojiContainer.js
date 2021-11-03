60ae065cb2ab8b5e65d4ac46a6b40e53df972cda
born3am <psmaranhao@gmail.com>
2021-11-03 09:24:21 +0100

    update

---
+++ src/components/EmojiContainer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function EmojiContainer() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div>
+            <h1>React Search Emoji <span>&#x1F984;</span></h1>
+            <p>Simple tool for search emoji with React</p>
+        
+
+        </div>
+    )
+}
